10c011663ff158d40a3144493ea531c24729adba
Edgars Irmejs <edgars.irmejs@gmail.com>
2014-04-20 00:15:22 +0300

    formatting

--- docs/capture_output.sh
+++ docs/capture_output.sh
@@ -2,4 +2,10 @@
 
 pid=${1?$'\n\t'"Usage: $0 <pid>"$'\n\t'"Captures the output of any running program."}
 
-strace -s1000000000 -e trace=write -q -p $pid 2>&1 | grep --line-buffered "^write([12]," | sed -u 's/^write([12], "\(.*\)", [0-9]*)\s*= [0-9]*$/\1/g' | sed -u 's/%/%%/g' | while IFS= read -r line; do printf "$line"; done
+strace -s1000000000 -e trace=write -q -p $pid 2>&1 | \
+	grep --line-buffered "^write([12]," | \
+	sed -u 's/^write([12], "\(.*\)", [0-9]*)\s*= [0-9]*$/\1/g' | \
+	sed -u 's/%/%%/g' | \
+	while IFS= read -r line; do
+		printf "$line"
+	done
