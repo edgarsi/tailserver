597d973b2bbc283f6461df57a6d89b0a224b166a
Edgars Irmejs <edgars.irmejs@gmail.com>
2014-05-06 01:19:59 +0300

    Allow passing extra params.

--- test/fast_server.sh
+++ test/fast_server.sh
@@ -1,3 +1,3 @@
 
 socket=${1?$'\n\t'"Usage: $0 <socket_file_name>"$'\n\t'"Launches tailserver and feeds input to it."}
-seq 1 1000000000 | tailserver $socket
+seq 1 1000000000 | tailserver $socket $2 $3 $4 $5 $6 $7 $8 $9
